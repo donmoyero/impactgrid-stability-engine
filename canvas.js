88ec2902b975592b93330a84132687d9441bf792
donmoyero <63630549+donmoyero@users.noreply.github.com>
2026-03-13 10:02:10 -0700

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,1 +1,33 @@
+function startRenderLoop(){
 
+function loop(){
+
+renderFrame();
+
+requestAnimationFrame(loop);
+
+}
+
+loop();
+
+}
+
+function renderFrame(){
+
+const ctx = Editor.ctx;
+
+ctx.clearRect(0,0,Editor.canvas.width,Editor.canvas.height);
+
+if(Editor.video){
+
+ctx.drawImage(
+Editor.video,
+0,
+0,
+Editor.canvas.width,
+Editor.canvas.height
+);
+
+}
+
+}
